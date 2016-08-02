dd5b83399c8d09655a46bb84f5ed808b590976a9
Peter Aguilar <peter.aguilar2287@gmail.com>
2016-08-02 14:28:23 -0600

    add crud routes

--- routes/work.js
+++ routes/work.js
@@ -9,4 +9,28 @@ router.get('/', function(req, res, next) {
   })
 });
 
+router.get('/:id', function(req, res, next) {
+  return knex('work').where('id', req.params.id).then(function(data) {
+  res.json(data);
+  })
+});
+
+router.post('/', function(req, res, next) {
+  return knex('work').insert(req.body).then(function(data) {
+  res.json({success: "submission excepted"});
+  })
+});
+
+router.put('/:id', function(req, res, next) {
+  return knex('work')update(req.body).where('id', req.params.id).then(function(data) {
+  res.json({success: "post edited"});
+  })
+});
+
+router.delete('/:id', function(req, res, next) {
+  return knex('work').del().where('id', req.params.id).then(function(data) {
+  res.json({success: "item deleted"});
+  })
+});
+
 module.exports = router;
